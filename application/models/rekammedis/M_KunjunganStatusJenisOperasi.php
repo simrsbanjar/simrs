e4670380a4d39d2c3fbd72653e164a86653e8d86
Programmer SIMRS <programmer@rsubanjar.local>
2021-03-29 08:19:26 +0700

    perbaikan query

--- application/models/rekammedis/M_KunjunganStatusJenisOperasi.php
+++ application/models/rekammedis/M_KunjunganStatusJenisOperasi.php
@@ -123,6 +123,18 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                              WHERE TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
                                                and '" . $tanggalakhir . " 23:59:59' 
                                                and KdInstalasi ='" . $instalasi . "'
+                                            UNION ALL
+                                            SELECT DISTINCT 
+                                                   TANGGAL = CONVERT(DATE,TglPendaftaran)
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien 
+                                             WHERE TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                               and '" . $tanggalakhir . " 23:59:59' 
+                                               and KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE B.TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                                                and '" . $tanggalakhir . " 23:59:59' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
                                                ORDER BY CONVERT(DATE,TglPendaftaran) ");
             } else if ($format == '2') {
                 $query = $this->db->query("SELECT DISTINCT 
@@ -133,7 +145,20 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                             and '" . $tanggalakhir . " 23:59:59' 
                                             and A.KdInstalasi ='" . $instalasi . "'
                                             AND A.Detail	= B.JenisOperasi
-                                            ORDER BY B.KdJenisOperasi, A.Detail ");
+                                        UNION ALL
+                                        SELECT DISTINCT 
+                                                KDKELOMPOK = CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                KELOMPOK = A.Detail
+                                        FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                        WHERE A.TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                            and '" . $tanggalakhir . " 23:59:59' 
+                                            and A.KdInstalasi ='" . $instalasi . "'
+                                            AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE B.TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                                                and '" . $tanggalakhir . " 23:59:59' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                            ORDER BY A.Detail ");
             } else {
                 $query = $this->db->query("SELECT DISTINCT 
                                                   TANGGAL = CONVERT(DATE,A.TglPendaftaran), 
@@ -146,7 +171,23 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                                and A.KdInstalasi ='" . $instalasi . "'
                                                AND A.Detail	= B.JenisOperasi
                                                GROUP BY CONVERT(DATE,A.TglPendaftaran),B.KdJenisOperasi,A.Detail
-                                            ORDER BY A.Detail,B.KdJenisOperasi,CONVERT(DATE,A.TglPendaftaran) ");
+                                            UNION ALL
+                                            SELECT DISTINCT 
+                                                  TANGGAL = CONVERT(DATE,A.TglPendaftaran), 
+                                                  KDKELOMPOK = CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                  KELOMPOK = A.Detail,
+                                                  JUMLAH	= SUM(A.JmlPasien) 
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                             WHERE A.TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                               and '" . $tanggalakhir . " 23:59:59' 
+                                               and A.KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE B.TglPendaftaran BETWEEN '" . $tanggalawal . " 00:00:00" . "' 
+                                                                and '" . $tanggalakhir . " 23:59:59' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                               GROUP BY CONVERT(DATE,A.TglPendaftaran),A.Detail
+                                            ORDER BY CONVERT(DATE,A.TglPendaftaran)  ");
             }
             // format bulan    
         } else if ($status == 2) {
@@ -171,7 +212,35 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                                and MONTH(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
                                                and '" . $tanggalakhir . "' 
                                                and KdInstalasi ='" . $instalasi . "'
-                                               ORDER BY CONVERT(CHAR(2),TglPendaftaran,101) ");
+                                               UNION ALL
+                                               SELECT DISTINCT 
+                                                   IDTANGGAL	= CONVERT(CHAR(2),TglPendaftaran,101),
+                                                   TANGGAL = (CASE WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '01' THEN 'JANUARI'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '02' THEN 'FEBRUARI'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '03' THEN 'MARET'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '04' THEN 'APRIL'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '05' THEN 'MEI'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '06' THEN 'JUNI'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '07' THEN 'JULI'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '08' THEN 'AGUSTUS'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '09' THEN 'SEPTEMBER'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '10' THEN 'OKTOBER'
+                                                                    WHEN CONVERT(CHAR(2),TglPendaftaran,101) = '11' THEN 'NOVEMBER'
+                                                                    ELSE 'DESEMBER'
+                                                                END)
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien 
+                                             WHERE YEAR(TglPendaftaran) = '$tahun'
+                                               and MONTH(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                               and '" . $tanggalakhir . "' 
+                                               and KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE YEAR(B.TglPendaftaran) = '$tahun'
+                                                                and MONTH(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                               ORDER BY CONVERT(CHAR(2),TglPendaftaran,101) 
+                                               ");
             } else if ($format == '2') {
                 $query = $this->db->query("SELECT DISTINCT 
                                                 KDKELOMPOK = B.KdJenisOperasi,
@@ -182,7 +251,23 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                             and '" . $tanggalakhir . "' 
                                             and A.KdInstalasi ='" . $instalasi . "'
                                             AND A.Detail	= B.JenisOperasi
-                                            ORDER BY B.KdJenisOperasi, A.Detail ");
+                                        UNION ALL
+                                        SELECT DISTINCT 
+                                                KDKELOMPOK = CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                KELOMPOK = A.Detail
+                                        FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                        WHERE YEAR(TglPendaftaran) = '$tahun'
+                                            and MONTH(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                            and '" . $tanggalakhir . "' 
+                                            and A.KdInstalasi ='" . $instalasi . "'
+                                            AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE YEAR(B.TglPendaftaran) = '$tahun'
+                                                                and MONTH(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                            ORDER BY A.Detail 
+                                        ");
             } else {
                 $query = $this->db->query("SELECT DISTINCT 
                                                   TANGGAL = CONVERT(CHAR(2),A.TglPendaftaran,101), 
@@ -196,7 +281,25 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                                and A.KdInstalasi ='" . $instalasi . "'
                                                AND A.Detail	= B.JenisOperasi
                                                GROUP BY CONVERT(CHAR(2),A.TglPendaftaran,101),B.KdJenisOperasi,A.Detail
-                                            ORDER BY A.Detail,B.KdJenisOperasi,CONVERT(CHAR(2),A.TglPendaftaran,101) ");
+                                            UNION ALL
+                                            SELECT DISTINCT 
+                                                  TANGGAL = CONVERT(CHAR(2),A.TglPendaftaran,101), 
+                                                  KDKELOMPOK =  CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                  KELOMPOK = A.Detail,
+                                                  JUMLAH	= SUM(A.JmlPasien) 
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                              WHERE YEAR(TglPendaftaran) = '$tahun'
+                                               and MONTH(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                               and '" . $tanggalakhir . "' 
+                                               and A.KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE  YEAR(B.TglPendaftaran) = '$tahun'
+                                                                and MONTH(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                               GROUP BY CONVERT(CHAR(2),A.TglPendaftaran,101),A.Detail
+                                            ORDER BY CONVERT(CHAR(2),A.TglPendaftaran,101)");
             }
             // format tahun
         } else {
@@ -207,6 +310,18 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                              WHERE YEAR(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
                                                and '" . $tanggalakhir . "' 
                                                and KdInstalasi ='" . $instalasi . "'
+                                               UNION ALL
+                                               SELECT DISTINCT 
+                                                   TANGGAL = CONVERT(CHAR(4),TglPendaftaran,102)
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien 
+                                             WHERE YEAR(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                               and '" . $tanggalakhir . "' 
+                                               and KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE YEAR(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
                                                ORDER BY CONVERT(CHAR(4),TglPendaftaran,102) ");
             } else if ($format == '2') {
                 $query = $this->db->query("SELECT DISTINCT 
@@ -217,7 +332,21 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                             and '" . $tanggalakhir . "' 
                                             and A.KdInstalasi ='" . $instalasi . "'
                                             AND A.Detail	= B.JenisOperasi
-                                            ORDER BY B.KdJenisOperasi, A.Detail ");
+                                        UNION ALL
+                                        SELECT DISTINCT 
+                                                KDKELOMPOK = CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                KELOMPOK = A.Detail
+                                        FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                        WHERE YEAR(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                            and '" . $tanggalakhir . "' 
+                                            and A.KdInstalasi ='" . $instalasi . "'
+                                            AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE YEAR(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                            ORDER BY A.Detail
+                                         ");
             } else {
                 $query = $this->db->query("SELECT DISTINCT 
                                                   TANGGAL = CONVERT(CHAR(4),A.TglPendaftaran,102), 
@@ -230,7 +359,24 @@ class M_KunjunganStatusJenisOperasi extends CI_Model
                                                and A.KdInstalasi ='" . $instalasi . "'
                                                AND A.Detail	= B.JenisOperasi
                                                GROUP BY CONVERT(CHAR(4),A.TglPendaftaran,102),B.KdJenisOperasi,A.Detail
-                                            ORDER BY A.Detail,B.KdJenisOperasi,CONVERT(CHAR(4),A.TglPendaftaran,102) ");
+                                        UNION ALL
+                                        SELECT DISTINCT 
+                                                  TANGGAL = CONVERT(CHAR(4),A.TglPendaftaran,102), 
+                                                  KDKELOMPOK = CASE WHEN A.Detail = 'Baru' THEN '01' ELSE '02' END,
+                                                  KELOMPOK = A.Detail,
+                                                  JUMLAH	= SUM(A.JmlPasien) 
+                                              FROM V_DataKunjunganPasienMasukBStatusPasien A
+                                              WHERE YEAR(TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                               and '" . $tanggalakhir . "' 
+                                               and A.KdInstalasi ='" . $instalasi . "'
+                                               AND NOT EXISTS (SELECT 1	
+                                                                FROM V_DataKunjunganPasienMasukIBSBJenisOperasi B
+                                                                WHERE YEAR(B.TglPendaftaran) BETWEEN '" . $tanggalawal . "' 
+                                                                and '" . $tanggalakhir . "' 
+                                                                and B.KdInstalasi ='" . $instalasi . "')
+                                               GROUP BY CONVERT(CHAR(4),A.TglPendaftaran,102),A.Detail
+                                            ORDER BY A.Detail,CONVERT(CHAR(4),A.TglPendaftaran,102)
+                                         ");
             }
         }
 
