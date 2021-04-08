a410626499bc9d832c36691cbe6ac30b7762cdf7
Programmer SIMRS <programmer@rsubanjar.local>
2021-04-08 09:26:37 +0700

    Penamaan periode pada grafik menjadi bulan bukan lagi nomer

--- application/controllers/KunjunganStatusRujukanPasien.php
+++ application/controllers/KunjunganStatusRujukanPasien.php
@@ -96,8 +96,60 @@ class KunjunganStatusRujukanPasien extends CI_Controller
         } elseif ($nilaifilter == 2) {
             $instalasi = $this->input->post('instalasi1');
 
+            if ($bulanawal == '1') {
+                $bulanawaltext = 'Januari';
+            } else if ($bulanawal == '2') {
+                $bulanawaltext = 'Februari';
+            } else if ($bulanawal == '3') {
+                $bulanawaltext = 'Maret';
+            } else if ($bulanawal == '4') {
+                $bulanawaltext = 'April';
+            } else if ($bulanawal == '5') {
+                $bulanawaltext = 'Mei';
+            } else if ($bulanawal == '6') {
+                $bulanawaltext = 'Juni';
+            } else if ($bulanawal == '7') {
+                $bulanawaltext = 'Juli';
+            } else if ($bulanawal == '8') {
+                $bulanawaltext = 'Agustus';
+            } else if ($bulanawal == '9') {
+                $bulanawaltext = 'September';
+            } else if ($bulanawal == '10') {
+                $bulanawaltext = 'Oktober';
+            } else if ($bulanawal == '11') {
+                $bulanawaltext = 'November';
+            } else {
+                $bulanawaltext = 'Desember';
+            }
+
+            if ($bulanakhir == '1') {
+                $bulanakhirtext = 'Januari';
+            } else if ($bulanakhir == '2') {
+                $bulanakhirtext = 'Februari';
+            } else if ($bulanakhir == '3') {
+                $bulanakhirtext = 'Maret';
+            } else if ($bulanakhir == '4') {
+                $bulanakhirtext = 'April';
+            } else if ($bulanakhir == '5') {
+                $bulanakhirtext = 'Mei';
+            } else if ($bulanakhir == '6') {
+                $bulanakhirtext = 'Juni';
+            } else if ($bulanakhir == '7') {
+                $bulanakhirtext = 'Juli';
+            } else if ($bulanakhir == '8') {
+                $bulanakhirtext = 'Agustus';
+            } else if ($bulanakhir == '9') {
+                $bulanakhirtext = 'September';
+            } else if ($bulanakhir == '10') {
+                $bulanakhirtext = 'Oktober';
+            } else if ($bulanakhir == '11') {
+                $bulanakhirtext = 'November';
+            } else {
+                $bulanakhirtext = 'Desember';
+            }
+
             $data['title'] = "Laporan Kunjungan Status dan Rujukan Pasien Berdasarkan Bulan";
-            $data['subtitle'] =  $bulanawal . ' s.d ' . $bulanakhir . ' Tahun : ' . $tahun1;
+            $data['subtitle'] =  $bulanawaltext . ' s.d ' . $bulanakhirtext . ' Tahun : ' . $tahun1;
             $data['instalasi'] = $instalasi;
 
             if ($format == '1') {
